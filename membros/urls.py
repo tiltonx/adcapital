595565e417c22f7cda61691be82dbc3ef547c3b9
C1277424 Diego Tilton da Silva Frazao <c1277424@interno.bb.com.br>
2026-03-25 17:23:11 -0300

    Fix API choices routing and remove duplication

--- membros/urls.py
+++ membros/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import MembroViewSet
+from .views import MembroViewSet, buscar_opcoes_funcao, buscar_opcoes_parentesco
 
 router = DefaultRouter()
-router.register(r'membros', MembroViewSet) # Isso cria a rota /api/membros/
+router.register(r'membros', MembroViewSet)
 
 urlpatterns = [
+    path('opcoes-funcao/', buscar_opcoes_funcao, name='opcoes-funcao'),
+    path('opcoes-parentesco/', buscar_opcoes_parentesco, name='opcoes-parentesco'),
     path('', include(router.urls)),
 ]
--- membros/views.py
+++ membros/views.py
@@ -1,6 +1,20 @@
 from rest_framework import viewsets
-from .models import Membro
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Membro, Parentesco
 from .serializers import MembroSerializer
+
+@api_view(['GET'])
+def buscar_opcoes_funcao(request):
+    """Retorna a lista dinâmica de funções extraída do models"""
+    opcoes = [{'id': f[0], 'nome': f[1]} for f in Membro.FUNCOES_CHOICES]
+    return Response(opcoes)
+
+@api_view(['GET'])
+def buscar_opcoes_parentesco(request):
+    """Retorna a lista dinâmica de graus de parentesco extraída do models"""
+    opcoes = [{'id': f[0], 'nome': f[1]} for f in Parentesco.GRAU_CHOICES]
+    return Response(opcoes)
 
 class MembroViewSet(viewsets.ModelViewSet):
     """
